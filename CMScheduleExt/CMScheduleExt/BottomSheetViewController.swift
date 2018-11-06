02c0c4dca72f35e2a94523d4bf9ff193bff7d8a3
Kevin Vo <kevin.vo@mac06-wa.local>
2018-11-06 14:24:18 -0700

    Added Subview Things

---
+++ CMScheduleExt/CMScheduleExt/BottomSheetViewController.swift
@@ -0,0 +1,60 @@
+//
+//  BottomSheetViewController.swift
+//  CMScheduleExt
+//
+//  Created by Kevin Vo on 2018-11-05.
+//  Copyright © 2018 CampusMAppTeam. All rights reserved.
+//
+
+import UIKit
+
+class BottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gesture = UIPanGestureRecognizer.init(target: self, action: #selector(BottomSheetViewController.panGesture))
+        view.addGestureRecognizer(gesture)        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareBackgroundView()
+        
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            let frame = self?.view.frame
+            let yComponent = UIScreen.main.bounds.height - 200
+            self?.view.frame = CGRect(x: 0, y: yComponent, width: frame!.width, height: frame!.height)
+        }
+    }
+    
+    func prepareBackgroundView(){
+        let blurEffect = UIBlurEffect.init(style: .dark)
+        let visualEffect = UIVisualEffectView.init(effect: blurEffect)
+        let bluredView = UIVisualEffectView.init(effect: blurEffect)
+        bluredView.contentView.addSubview(visualEffect)
+        
+        visualEffect.frame = UIScreen.main.bounds
+        bluredView.frame = UIScreen.main.bounds
+        
+        view.insertSubview(bluredView, at: 0)
+    }
+    
+    @objc func panGesture(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        let y = self.view.frame.minY
+        self.view.frame = CGRect(x: 0, y: y + translation.y, width: view.frame.width, height: view.frame.height)
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CMScheduleExt/CMScheduleExt/ViewController.swift
+++ CMScheduleExt/CMScheduleExt/ViewController.swift
@@ -13,8 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addBottomSheetView()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       addBottomSheetView()
+    }
+    
+    func addBottomSheetView() {
+        let bottomVC = storyboard!.instantiateViewController(withIdentifier: "BottomView")
+        
+        self.addChild(bottomVC)
+        self.view.addSubview(bottomVC.view)
+        bottomVC.didMove(toParent: self)
+        
+        let height = view.frame.height
+        let width = view.frame.width
+        bottomVC.view.frame = CGRect(x: 0, y: self.view.frame.maxY, width: width, height: height)
+    }
+    
 }
 
