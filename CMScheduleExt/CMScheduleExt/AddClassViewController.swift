17a1b4a64e5e60e8849903697954b40559e205a2
zachalbers <back2zach2@gmail.com>
2018-11-22 18:18:48 -0700

    Added dropdown table with lectures and labs

--- CMScheduleExt/CMScheduleExt/AddClassViewController.swift
+++ CMScheduleExt/CMScheduleExt/AddClassViewController.swift
@@ -18,6 +18,8 @@ class AddClassViewController: UIViewController, UITableViewDataSource, UITableVi
     
     
     var searchActive : Bool = false
+    var selected: IndexPath = IndexPath.init()
+    var selectedScroller: ASHorizontalScrollView? = nil
     var data = ["CPSC 313","CPSC 331","CPSC 575", "ECON 201", "ARKY 201", "ARKY 417", "INTR 501", "BIST 600", "CHIN 317", "MUSI 211", "CHEM 301"]
     var classSections: [String: Array<String>] = [:]
 
@@ -36,6 +38,8 @@ class AddClassViewController: UIViewController, UITableViewDataSource, UITableVi
         tableView.delegate = self
         tableView.dataSource = self
         searchBar.delegate = self
+        
+        
 
     }
     
@@ -103,18 +107,33 @@ class AddClassViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(searchActive) {
+        if(searchActive || !selected.isEmpty) {
             return filtered.count
         }
         return 0;
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+
+        if((!selected.isEmpty) && indexPath.row == selected.row) {
+            return 150
+        } else {
+        return 40
+        }
+    }
+
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell;
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? ClassTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+
+        
         if(searchActive){
-            cell.textLabel?.text = filtered[indexPath.row]
-        }        
-        return cell;
+            
+            cell.courseName.text = filtered[indexPath.row]
+        }
+    return cell;
     }
 
     @IBAction func undweaadosfj(_ sender: Any) {
@@ -130,43 +149,104 @@ class AddClassViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        searchBar.resignFirstResponder()
         
         //optional, to get from any UIButton for example
         
-        let indexPath = tableView.indexPathForSelectedRow
-        let cell = tableView.cellForRow(at: indexPath!)
-        let cellText = cell!.textLabel!.text ?? "Default Class"
-        
-        
-        
-        let optionMenu = UIAlertController(title: nil, message: cellText, preferredStyle: .actionSheet)
-        
-       
-        for sectionName in classSections[cellText]! {
-            optionMenu.addAction(UIAlertAction(title: sectionName, style: .default){ _ in
-                        self.showAlert()
-                        print(cellText)
-                        print(sectionName)
-                
+        let indexPath_ = tableView.indexPathForSelectedRow!
+        let cell = tableView.cellForRow(at: indexPath_) as! ClassTableViewCell
+        let cellName = cell.courseName.text
 
-                        })
+        
+        if (selectedScroller != nil) {
+            selectedScroller?.removeFromSuperview()
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let horizontalScrollView:ASHorizontalScrollView = ASHorizontalScrollView(frame:CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 60))
+        //for all other screen sizes that doesn't set here, it would use defaultMarginSettings instead
+        horizontalScrollView.defaultMarginSettings = MarginSettings(leftMargin: 10, miniMarginBetweenItems: 10, miniAppearWidthOfLastItem: 20)
         
-        // 4
+        horizontalScrollView.uniformItemSize = CGSize(width: 80, height: 50)
+        //this must be called after changing any size or margin property of this class to get acurrate margin
+        horizontalScrollView.setItemsMarginOnce()
+        
+        for section in classSections[cellName!]!{
+            let button = UIButton(frame: CGRect.zero)
+            button.setTitle(section, for: .normal)
+            let backColor = UIColor(red: 182/255, green: 209/255, blue: 146/255, alpha: 1)
+            button.backgroundColor = backColor
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.layer.cornerRadius = 5
+            button.titleLabel!.font = UIFont(name: "AppleSDGothicNeo-Thin" , size: 20)
+            button.addTarget(self, action: #selector(sectionSelected), for: .touchUpInside)
+            
+            
+            horizontalScrollView.addItem(button)
+        }
+        
+       
+        
+        cell.contentView.addSubview(horizontalScrollView)
+        horizontalScrollView.translatesAutoresizingMaskIntoConstraints = false
+        cell.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell.contentView, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell.contentView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 40))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 60))
+        cell.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell.contentView, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0))
+        
+        tableView.reloadData()
+        
+        selectedScroller = horizontalScrollView
 
-        optionMenu.addAction(cancelAction)
+        selected = indexPath
+        searchBar.resignFirstResponder()
         
         
-//        let action = UIAlertAction(title: "Save", style: .default){ _ in
-//            print("We can run a block of code." )
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        
+        
+
+
+        
+
+        
+//
+//
+//
+//        let optionMenu = UIAlertController(title: nil, message: cellText, preferredStyle: .actionSheet)
+//
+//
+//        for sectionName in classSections[cellText]! {
+//            optionMenu.addAction(UIAlertAction(title: sectionName, style: .default){ _ in
+//                        self.showAlert()
+//                        print(cellText)
+//                        print(sectionName)
+//
+//
+//                        })
 //        }
-//        optionMenu.addAction(action)
+//
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+//
+//        // 4
+//
+//        optionMenu.addAction(cancelAction)
+//
+//
+////        let action = UIAlertAction(title: "Save", style: .default){ _ in
+////            print("We can run a block of code." )
+////        }
+////        optionMenu.addAction(action)
+//
+//        // 5
+//        self.present(optionMenu, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    @objc func sectionSelected(sender: UIButton!) {
+        showAlert()
         
-        // 5
-        self.present(optionMenu, animated: true, completion: nil)
     }
     
     /*
