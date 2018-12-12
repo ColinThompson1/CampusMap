cf05c183afaed5c008700acc058abce56d390d9f
Xian-Meng Low <xmlow@XM-Macbook.local>
2018-12-12 14:57:26 -0700

    Added NSOBject and Coding functions

--- CMScheduleExt/CMScheduleExt/Class.swift
+++ CMScheduleExt/CMScheduleExt/Class.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import os.log
 
-class Class{
+class Class: NSObject, NSCoding{
+    
+    //MARK: Properties
     
     var name: String
     var type: String
@@ -17,6 +20,19 @@ class Class{
     var room: String
     
     let dateFormatter = DateFormatter()
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("classes")
+    
+    //MARK: Types
+    
+    struct PropertyKey{
+        static let name = "name"
+        static let type = "type"
+        static let semester = "semester"
+        static let days = "days"
+        static let room = "room"
+    }
     
     init? (name: String, type: String,  semester: [String:String], days: [String:String], room: String){
         
@@ -63,5 +79,34 @@ class Class{
         let timeSplitter = dayString?.components(separatedBy: " - ")
         
         return dateFormatter.date(from:(timeSplitter?[1])!)!
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(type, forKey: PropertyKey.type)
+        aCoder.encode(semester, forKey: PropertyKey.semester)
+        aCoder.encode(days, forKey: PropertyKey.days)
+        aCoder.encode(room, forKey: PropertyKey.room)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Class object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let type = aDecoder.decodeObject(forKey: PropertyKey.type) as! String
+        
+        let semester = aDecoder.decodeObject(forKey: PropertyKey.semester) as! [String:String]
+        
+        let days = aDecoder.decodeObject(forKey: PropertyKey.days) as! [String:String]
+        
+        let room = aDecoder.decodeObject(forKey: PropertyKey.room) as! String
+        
+        self.init(name: name, type: type, semester: semester, days: days, room: room)
+        
     }
 }
