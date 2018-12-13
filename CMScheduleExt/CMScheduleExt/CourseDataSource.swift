b9e38159dbcc220566ccca0f2a3e8b971fcdfc9b
zachalbers <back2zach2@gmail.com>
2018-12-12 20:58:50 -0700

    Revert "Merge branch 'Zach-GUI' of https://github.com/ColinThompson1/CampusMap into Zach-GUI"
    This reverts commit 3e96d56fce76202ab1eb89a6b10c0c2518acfc0e, reversing
    changes made to de017e5837cdc6d9d3d783cd8eeef9e9d0418817.

--- CMScheduleExt/CMScheduleExt/CourseDataSource.swift
+++ CMScheduleExt/CMScheduleExt/CourseDataSource.swift
@@ -10,14 +10,14 @@ import PromiseKit
 import Foundation
 
 class CourseDataSource {
-
+    
     static let shared = CourseDataSource()
-
+    
     private var courses: [Int : [String : Course]] = [:]
-
+    
     private init() {}
-
-
+    
+    
     func getCourses(semesterID: Int) -> Promise<[String : Course]> {
         // If we already got the data, use that.
         if (hasLoaded(semesterID: semesterID)) {
@@ -32,35 +32,35 @@ class CourseDataSource {
                 })
         }
     }
-
+    
     func hasLoaded(semesterID: Int) -> Bool {
         return courses[semesterID] != nil
     }
-
-//    func convertAll(cs: OCRCourses, semesterID: Int) -> Promise<[Class?]> {
-//        return getCourses(semesterID: semesterID).compactMap { (formalCourses: [String : Course]) -> [Class?] in
-//            var converted = [Class?]()
-//            for c in cs.values {
-//                converted.append(
-//                    CourseDataSource.convert(ocrCourse: c, formalCourses: formalCourses)
-//                )
-//            }
-//            return converted
-//        }
-//    }
-
+    
+    func convertAll(cs: OCRCourses, semesterID: Int) -> Promise<[Class?]> {
+        return getCourses(semesterID: semesterID).compactMap { (formalCourses: [String : Course]) -> [Class?] in
+            var converted = [Class?]()
+            for c in cs.values {
+                converted.append(
+                    CourseDataSource.convert(ocrCourse: c, formalCourses: formalCourses)
+                )
+            }
+            return converted
+        }
+    }
+    
     static func convert(ocrCourse: OCRCourse, formalCourses: [String : Course]) -> Class? {
         let tag = "\(ocrCourse.courseCode) \(ocrCourse.courseNum)"
-
+        
         guard let formalCourse: Course = formalCourses[tag] else {
             print("Could not find course from ocr")
             return Optional.none
         }
-
+        
         let letter = ocrCourse.section.replacingOccurrences(of: "[0-9]", with: "", options: [.regularExpression])
-
+        
         let number = String(ocrCourse.section.split(separator: "T")[0])
-
+        
         var type: ClassType
         switch letter {
             case "T":
@@ -71,19 +71,19 @@ class CourseDataSource {
                 //todo add Seminar to here and ocr
                 type = ClassType.lecture
         }
-
+        
         guard let periodic = formalCourse.periodics["\(type.rawValue) \(number)"] else {
             print("Could not find periodic from ocr")
             return Optional.none
         }
-
+        
         var days: [String : String] = [:]
         var semesters: [String : String] = [:]
         for tp: TimePeriod in periodic.timePeriods {
             days[tp.day.rawValue] = tp.time
             semesters[tp.day.rawValue] = tp.date
         }
-
+        
         return Class(name: tag, type: type.rawValue, semester: semesters, days: days, room: periodic.room)
     }
 }
